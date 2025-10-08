0cabb4c7753a0aa31c3d433af03eb9c913fa337c
Pedro-Spindola <118736460+Pedro-Spindola@users.noreply.github.com>
2025-10-07 21:52:10 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 const saudacao = require('./hello');
 // Teste com erro intencional
-const resultado = saudacao("Mundo");
+const resultado = saudacao("Olá, Mundo!");
 const esperado = "Olá, Mundo!";
 if (resultado !== esperado) {
-console.error(`❌ FALHA: esperado "${esperado}", mas recebeu "${resultado}"`);
-process.exit(1); // Faz o CI falhar
+  console.error(`❌ FALHA: esperado "${esperado}", mas recebeu "${resultado}"`);
+  process.exit(1); // Faz o CI falhar
 }
 console.log("✅ Todos os testes passaram!");
